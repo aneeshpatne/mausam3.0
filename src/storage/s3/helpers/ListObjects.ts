720f4802b3012882a0f3ccc2a42f0060da06584b
aneeshpatne <aneeshpatne@gmail.com>
2026-04-02 08:21:41 +0000

    feat: enhance ListObjectsFromBuckets to return sorted objects and handle errors gracefully

--- src/storage/s3/helpers/ListObjects.ts
+++ src/storage/s3/helpers/ListObjects.ts
@@ -2,13 +2,21 @@ import { ListObjectsV2Command, S3ServiceException } from "@aws-sdk/client-s3";
 import { client } from "../client/s3";
 export async function ListObjectsFromBuckets(bucket: string) {
   try {
-    const res = await client.send(new ListObjectsV2Command({ Bucket: bucket }));
-    console.log(res?.Contents);
+    const { Contents = [] } = await client.send(
+      new ListObjectsV2Command({ Bucket: bucket }),
+    );
+    const sorted = Contents.sort(
+      (a, b) =>
+        new Date(b.LastModified!).getTime() -
+        new Date(a.LastModified!).getTime(),
+    );
+    return sorted;
   } catch (e) {
     if (e instanceof S3ServiceException) {
       console.error(
         `Error from S3 while listing objects.  ${e.name}: ${e.message}`,
       );
+      return [];
     } else {
       throw e;
     }
--- src/storage/s3/helpers/uploadWithLimit.ts
+++ src/storage/s3/helpers/uploadWithLimit.ts
@@ -1,6 +1,15 @@
 import { client } from "../client/s3";
+import { ListBuckets } from "./ListBuckets";
+import { ListObjectsFromBuckets } from "./ListObjects";
 
-export async function UploadWithLimit(
-  bucketName: string,
-  imageBuffer: Buffer,
-) {}
+// bucketName: string, imageBuffer: Buffer
+export async function UploadWithLimit() {
+  const bucket = await ListObjectsFromBuckets("radar-max-z");
+  console.log(bucket.length);
+  if (bucket.length > 0) {
+    const first = bucket[0];
+    const last = bucket[bucket.length - 1];
+  }
+}
+
+await UploadWithLimit();
