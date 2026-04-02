7c83e405a695be0f69ab70bd348e40bb0d0b1dc2
aneeshpatne <aneeshpatne@gmail.com>
2026-04-02 06:14:22 +0000

    feat: configure S3 client with region, endpoint, and credentials

--- src/storage/s3/s3.ts
+++ src/storage/s3/s3.ts
@@ -1,3 +1,10 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-const client = new S3Client({});
+const client = new S3Client({
+  region: "auto",
+  endpoint: process.env.ENDPOINT_URL,
+  credentials: {
+    accessKeyId: process.env.aws_access_key_id!,
+    secretAccessKey: process.env.aws_secret_access_key!,
+  },
+});
