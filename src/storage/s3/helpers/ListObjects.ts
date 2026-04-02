5ee18f9ceb52e5a168e760bbc38dd52be1afbcdb
aneeshpatne <aneeshpatne@gmail.com>
2026-04-02 06:59:37 +0000

    refactor: update ListObjectsFromBuckets and FindLatestObjectFromBucket to accept bucket parameter

--- src/storage/s3/helpers/ListObjects.ts
+++ src/storage/s3/helpers/ListObjects.ts
@@ -1,10 +1,8 @@
 import { ListObjectsV2Command, S3ServiceException } from "@aws-sdk/client-s3";
 import { client } from "../client/s3";
-export async function ListObjectsFromBuckets() {
+export async function ListObjectsFromBuckets(bucket: string) {
   try {
-    const res = await client.send(
-      new ListObjectsV2Command({ Bucket: "radar-max-z" }),
-    );
+    const res = await client.send(new ListObjectsV2Command({ Bucket: bucket }));
     console.log(res?.Contents);
   } catch (e) {
     if (e instanceof S3ServiceException) {
@@ -17,10 +15,10 @@ export async function ListObjectsFromBuckets() {
   }
 }
 
-export async function FindLatestObjectFromBucket() {
+export async function FindLatestObjectFromBucket(bucket: string) {
   try {
     const { Contents = [] } = await client.send(
-      new ListObjectsV2Command({ Bucket: "radar-max-z" }),
+      new ListObjectsV2Command({ Bucket: bucket }),
     );
 
     const latest = Contents.sort(
