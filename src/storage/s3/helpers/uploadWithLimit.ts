e139694cf4a23be43508963be300578c6f3299af
aneeshpatne <aneeshpatne@gmail.com>
2026-04-02 12:14:02 +0000

    Update uploadWithLimit buffer comparison flow

--- src/storage/s3/helpers/uploadWithLimit.ts
+++ src/storage/s3/helpers/uploadWithLimit.ts
@@ -1,16 +1,16 @@
+import { areBuffersSame } from "../../../data/radar/are-same";
 import { fetchImage } from "../../../data/radar/get-image";
 import { client } from "../client/s3";
 import { ListObjectsFromBuckets } from "./ListObjects";
 
 // bucketName: string, imageBuffer: Buffer
-export async function UploadWithLimit(bucketName: string) {
+export async function UploadWithLimit(bucketName: string, imageBuffer: Buffer) {
   const bucket = await ListObjectsFromBuckets(bucketName);
   console.log(bucket.length);
   if (bucket.length > 0) {
     const last = bucket[bucket.length - 1];
     const url = process.env.R2_PUBLIC_BASE_URL + bucketName + "/" + last?.Key;
-    console.log(await fetchImage(url));
+    const oldImageBuffer = await fetchImage(url);
+    areBuffersSame(imageBuffer, oldImageBuffer);
   }
 }
-
-await UploadWithLimit("radar-max-z");
