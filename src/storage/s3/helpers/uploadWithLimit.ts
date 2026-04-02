f0c91208f624a47f6d2db82152d8bfa211253960
aneeshpatne <aneeshpatne@gmail.com>
2026-04-02 08:39:34 +0000

    feat: update UploadWithLimit function to accept bucket name as parameter

--- src/storage/s3/helpers/uploadWithLimit.ts
+++ src/storage/s3/helpers/uploadWithLimit.ts
@@ -1,14 +1,16 @@
+import { fetchImage } from "../../../data/radar/get-image";
 import { client } from "../client/s3";
-import { ListBuckets } from "./ListBuckets";
 import { ListObjectsFromBuckets } from "./ListObjects";
 
 // bucketName: string, imageBuffer: Buffer
-export async function UploadWithLimit() {
-  const bucket = await ListObjectsFromBuckets("radar-max-z");
+export async function UploadWithLimit(bucketName: string) {
+  const bucket = await ListObjectsFromBuckets(bucketName);
   console.log(bucket.length);
   if (bucket.length > 0) {
     const last = bucket[bucket.length - 1];
+    const url = process.env.R2_PUBLIC_BASE_URL + bucketName + "/" + last?.Key;
+    console.log(await fetchImage(url));
   }
 }
 
-await UploadWithLimit();
+await UploadWithLimit("radar-max-z");
