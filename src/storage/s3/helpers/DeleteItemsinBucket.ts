aedacdee0fb37b17da42952a625b09108c8ae676
aneeshpatne <aneeshpatne@gmail.com>
2026-04-02 08:05:15 +0000

    feat: rename and export DeleteObjectFrom function to DeleteObjectFromBucket

--- src/storage/s3/helpers/DeleteItemsinBucket.ts
+++ src/storage/s3/helpers/DeleteItemsinBucket.ts
@@ -1,7 +1,10 @@
 import { DeleteObjectCommand, S3ServiceException } from "@aws-sdk/client-s3";
 import { client } from "../client/s3";
 
-async function DeleteObjectFrom(name: string, bucket_name: string) {
+export async function DeleteObjectFromBucket(
+  name: string,
+  bucket_name: string,
+) {
   try {
     const response = await client.send(
       new DeleteObjectCommand({ Bucket: bucket_name, Key: name }),
