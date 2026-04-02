4f6e8de38fb3134d679367a88128a599b0a5812a
aneeshpatne <aneeshpatne@gmail.com>
2026-04-02 12:15:02 +0000

    refactor: optimize image fetching and uploading process

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,7 +1,9 @@
 import { fetchImageAsJpeg } from "./data/radar/get-image";
 import { images } from "./data/radar/radar-image";
+import { UploadWithLimit } from "./storage/s3/helpers/uploadWithLimit";
 
 for (const image_obj of images) {
   console.log(image_obj.url);
-  console.log(await fetchImageAsJpeg(image_obj.url));
+  const imageBuffer = await fetchImageAsJpeg(image_obj.url);
+  await UploadWithLimit(image_obj.bucket_name, imageBuffer);
 }
