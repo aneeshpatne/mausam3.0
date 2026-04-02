7a6d6bfcb4a6a66e5581a6c61f0e5a874733e449
aneeshpatne <aneeshpatne@gmail.com>
2026-04-02 12:14:08 +0000

    Narrow buffer hashing inputs to array views

--- src/data/radar/are-same.ts
+++ src/data/radar/are-same.ts
@@ -1,5 +1,8 @@
-export function areBuffersSame(a: ArrayBuffer, b: ArrayBuffer): boolean {
-  const hashA = Bun.hash(a);
-  const hashB = Bun.hash(b);
+export function areBuffersSame(
+  a: ArrayBufferView,
+  b: ArrayBufferView,
+): boolean {
+  const hashA = Bun.hash(new Uint8Array(a.buffer, a.byteOffset, a.byteLength));
+  const hashB = Bun.hash(new Uint8Array(b.buffer, b.byteOffset, b.byteLength));
   return hashA === hashB;
 }
