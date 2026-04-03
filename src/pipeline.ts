71be7224cbc5bd5d82002abca8b38913b51c8193
aneeshpatne <aneeshpatne@gmail.com>
2026-04-03 07:26:01 +0000

    feat: add functionality to list saved images after AI summarization

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,5 +1,6 @@
 import { fetchImageAsJpeg } from "./data/radar/get-image";
 import { images } from "./data/radar/radar-image";
+import { ListObjectsFromBuckets } from "./storage/s3/helpers/ListObjects";
 import { UploadWithLimit } from "./storage/s3/helpers/uploadWithLimit";
 
 export const state = {
@@ -18,6 +19,14 @@ export async function runPipeline() {
     return;
   }
   console.log("Images have changed proceeding with AI summarization");
+  const saved_images: string[] = [];
+  for (const image_obj of images) {
+    var img_url = (await ListObjectsFromBuckets(image_obj.bucket_name))
+      .map((item) => item.Key)
+      .filter((key): key is string => key !== undefined);
+    saved_images.push(...img_url);
+  }
+  console.log(saved_images);
 }
 
 await runPipeline();
